5844c742e0ca9c818ee07d0d7617fc4d7421de2d
KingOW <kingow@163.com>
2021-06-07 14:57:42 +0800

    tijiao

--- medfut-wechat-service/src/main/java/com/medfut/wechat/web/PubWeChatController.java
+++ medfut-wechat-service/src/main/java/com/medfut/wechat/web/PubWeChatController.java
@@ -27,18 +27,22 @@ public class PubWeChatController {
         try {
             return this.pubWeChatService.savePubWeChat(pubWeChat);
         } catch (WeChatException e) {
-            return new ResultBean<>(false, ResultStatus.SERVER_ERROR, "添加微信公众平台账号失败:服务器出现异常.");
+            return new ResultBean<>(false, ResultStatus.SERVER_ERROR.getCode(), "添加微信公众平台账号失败:服务器出现异常.");
         }
     }
 
     /**
      * 查询微信公众账号信息：精确查询
+     *
      * @param weChatKey
      * @return
      */
     @GetMapping("/{weChatKey}")
-    public ResultBean<String> queryPubWeChat(@PathVariable(value = "weChatKey") String weChatKey) {
-
-        return null;
+    public ResultBean<PubWeChat> queryPubWeChat(@PathVariable(value = "weChatKey") String weChatKey) {
+        try {
+            return this.pubWeChatService.queryPubWeChatByKey(weChatKey);
+        } catch (WeChatException e) {
+            return new ResultBean<PubWeChat>(false, ResultStatus.SERVER_ERROR.getCode(), "查询微信公众账号失败:服务器出现异常.");
+        }
     }
 }
